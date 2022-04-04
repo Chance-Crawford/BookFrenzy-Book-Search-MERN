30ca6e3e021ec435fdc3266012c89a37f4f89a8a
Chance <chancecrawford30@gmail.com>
2022-04-03 17:09:25 -0700

    added save book and remove book resolver

--- Develop/server/schemas/resolvers.js
+++ Develop/server/schemas/resolvers.js
@@ -36,6 +36,65 @@ const resolvers = {
             // This matches our Auth type we defined.
             return { token, user };
         },
+
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            // uses bcrypt to compare the incoming password with the hashed password
+            const correctPw = await user.isCorrectPassword(password);
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            // give the user that is logging in a token
+            const token = signToken(user);
+
+            return { token, user };
+        },
+
+        saveBook: async (parent, args, context) => {
+            if(context.user){
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $addToSet: { savedBooks: args } },
+                    // return back updated user object and run the 
+                    // validators to make sure that the book being saved is a
+                    // valid book
+                    { new: true, runValidators: true }
+                );
+
+                if (!updatedUser) {
+                    throw new AuthenticationError('Could not update this user\'s books');
+                }
+
+                return updatedUser;
+            }
+
+            throw new AuthenticationError('You need to be logged in!');
+        },
+
+        // remove book from user's saved list.
+        removeBook: async (parent, { bookId }, context) => {
+            if(context.user){
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: { savedBooks: { bookId } } },
+                    { new: true }
+                );
+
+                if (!updatedUser) {
+                    throw new AuthenticationError('Could not update this user\'s books');
+                }
+
+                return updatedUser;
+            }
+
+            throw new AuthenticationError('You need to be logged in!');
+        }
     }
 };
 
